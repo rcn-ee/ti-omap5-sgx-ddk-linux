97c89b9883bcacaf96e467d7b20023b9c3d516c8
Anand Balagopalakrishnan <anandb@ti.com>
2015-08-13 12:53:43 +0530

    km: fix recursive mutex lock problem when debug options are enabled
    Signed-off-by: Anand Balagopalakrishnan <anandb@ti.com>

--- eurasia_km/services4/srvkm/env/linux/mmap.c
+++ eurasia_km/services4/srvkm/env/linux/mmap.c
@@ -1372,7 +1372,7 @@ MMapVOpenExt(struct vm_area_struct* ps_vma)
 			FindOffsetStructByPID(priv, OSGetCurrentProcessIDKM());
 	if (WARN_ON(!psOffsetStruct))
 		return;
-	LinuxLockMutex(&g_sMMapMutex);
+	LinuxLockMutexNested(&g_sMMapMutex, PVRSRV_LOCK_CLASS_MMAP);
 	MMapVOpenNoLock(ps_vma, psOffsetStruct);
 	LinuxUnLockMutex(&g_sMMapMutex);
 }
@@ -1386,7 +1386,7 @@ MMapVCloseExt(struct vm_area_struct* ps_vma)
 			FindOffsetStructByPID(priv, OSGetCurrentProcessIDKM());
 	if (WARN_ON(!psOffsetStruct))
 		return;
-	LinuxLockMutex(&g_sMMapMutex);
+	LinuxLockMutexNested(&g_sMMapMutex, PVRSRV_LOCK_CLASS_MMAP);
 	MMapVCloseNoLock(ps_vma, psOffsetStruct);
 
 	drm_gem_vm_close(ps_vma);
@@ -1414,7 +1414,7 @@ PVRMMapExt(struct file* pFile, struct vm_area_struct* ps_vma)
 
     PVR_UNREFERENCED_PARAMETER(pFile);
 
-    LinuxLockMutex(&g_sMMapMutex);
+    LinuxLockMutexNested(&g_sMMapMutex, PVRSRV_LOCK_CLASS_MMAP);
 
     ui32ByteSize = ps_vma->vm_end - ps_vma->vm_start;
 
